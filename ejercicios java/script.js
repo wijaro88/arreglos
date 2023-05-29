e8c3ba4c763c0ff2028a6c15ea72add03a4a4efb
Wilson R <wijaro88@gmail.com>
2023-05-29 18:12:40 -0500

    commit 1 ejercicios de java

---
+++ ejercicios java/script.js
@@ -0,0 +1,112 @@
+
+// alert("el js esta ligado")
+
+// EJERCICIO 1
+
+// let bel = prompt("¿Eres Bellisimo?");
+// if (bel =="si" || "SI"){
+//     console.log("Ciertamente")
+// }else{
+//     console.log("No te creo")
+// };
+
+// EJERCICIO 2
+
+// let num = parseInt ( prompt ("Indica un numero para saber si es divisible por 2"));
+// if (num % 2 == 0){
+//     console.log(num + " es divisible entre 2")
+// }else{
+//     console.log(num + " No es divisible entre 2")
+// };
+
+// EJERCICIO 3
+
+// let num2 = parseInt ( prompt ("Indica el numero para saber si es par o impar"));
+// if (num2 % 2 == 0){
+//     alert("PAR")
+// }else{
+//     alert("IMPAR")
+// };
+
+// // EJERCICIO 4
+
+// let num3 = parseInt ( prompt ("Indique un numero para la RIFA!!!!!!: "));
+// if (num3 == "1000"){
+//     console.log("GANASTE UN PREMIO!!!!!!!!!!")
+// }else{
+//     console.log("LO SENTIMOS, SIGUE PARTICIPANDO")
+// };
+
+//EJERCICIO 5
+
+// alert("programa para comparar dos numeros")
+// let nume1 = parseInt ( prompt (" Indica el primer numero para saber si es Mayor o Menor: " ));
+// let nume2 = parseInt ( prompt (" Indica el segundo numero para realizar la comparación: "));
+// if (nume1 >= nume2){
+//     alert("el numero:  " +nume1 + "  es mayor que el numero:  "+nume2 )
+// }else{
+//     alert("el numero:  " +nume2 + "  es mayor que el numero:  "+nume1)
+// };
+
+//EJERCICIO 6
+
+// alert("programa para comparar TRES numeros")
+// let nume1 = parseInt(prompt(" Indica el PRIMER numero para realizar la comparación: "));
+// let nume2 = parseInt(prompt(" Indica el SEGUNDO numero para realizar la comparación: "));
+// let nume3 = parseInt(prompt(" Indica el TERCER numero para realizar la comparación: "));
+// if (nume1 >= nume2) {
+//     if (nume1 >= nume3) {
+//         alert("el numero mayor es: " +nume1);
+//     } else { alert("el numero mayor es: " +nume3);} 
+// }    
+// else {if (nume2 >= nume3) {
+//         alert("el numero mayor es el: " + nume2);
+//     }
+//     else {
+//         alert("el numero mayor es el numero: " + nume3)
+//     } 
+//     }
+
+// EJERCICIO 7
+
+// let dia = prompt("indique el dia de la semana:  ")
+
+// switch
+// (dia) {
+
+//     case
+//         "lunes":
+//         alert("Hello lunes")
+//         break;
+
+//     case
+//         "martes":
+//         alert("Welcome martes")
+//         break;
+
+//     case
+//         "miercoles":
+//         alert("Hello miercoles")
+//         break;
+
+//     case
+//         "jueves":
+//         alert("Welcome jueves")
+//         break;
+
+//     case
+//         "viernes":
+//         alert("Hello viernes")
+//         break;
+
+//     case
+//         "sabado":
+//         alert("Welcome sabado")
+//         break;
+//     case
+//         "domingo":
+//         alert("Hello domingo")
+//         break;
+// }
+
+// EJERCICIO 8
